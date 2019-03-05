0e4aa8decc2ad64ff93aba79bd391b04eb019d08
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-03-04 16:02:12 -0800

    adding expected error

--- checker/tests/determinism/Issue81.java
+++ checker/tests/determinism/Issue81.java
@@ -3,6 +3,7 @@ import org.checkerframework.checker.determinism.qual.*;
 
 public class Issue81 {
     public static void f() {
+        // :: error: (assignment.type.incompatible)
         @Det int c = Objects.hashCode(new Object());
     }
 }
