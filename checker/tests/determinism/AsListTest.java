b05d84079790d25e5cbfad0355bb74484223bfb8
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-04-04 16:16:10 -0700

    Correctly annotates Arrays.asList; Fixes #93

--- checker/tests/determinism/AsListTest.java
+++ checker/tests/determinism/AsListTest.java
@@ -1,8 +1,8 @@
-// @skip-test
 import java.util.*;
 import org.checkerframework.checker.determinism.qual.*;
 
 public class AsListTest {
+
     public static void f(@NonDet Integer y) {
         @Det List<@Det String> list = Arrays.<@Det String>asList("");
         @NonDet List<@NonDet Integer> intList = Arrays.<@NonDet Integer>asList(y);
