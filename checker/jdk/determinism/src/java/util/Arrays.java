caf5e0bcedab17c5a15f3449aea95e8549488e28
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-04-04 14:18:52 -0700

    asList changes

--- checker/jdk/determinism/src/java/util/Arrays.java
+++ checker/jdk/determinism/src/java/util/Arrays.java
@@ -3798,7 +3798,7 @@ public class Arrays {
      */
     @SafeVarargs
     @SuppressWarnings("varargs")
-    public static <T> @PolyDet List<T> asList(@PolyDet T... a) {
+    public static <@PolyDet T> @PolyDet List<@PolyDet T> asList(@PolyDet T... a) {
         return new ArrayList<>(a);
     }
 
