da1e725ae19530c6be6c3a57643cb1592abe5aaf
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-04-04 14:53:49 -0700

    correct annotation for asList; Fixes #93

--- checker/tests/determinism/ArrayInits.java
+++ checker/tests/determinism/ArrayInits.java
@@ -5,7 +5,6 @@ import org.checkerframework.checker.determinism.qual.*;
 
 public class ArrayInits {
     void method() {
-        // :: error: (invalid.upper.bound.on.type.argument)
         @Det Object @Det [] objects = new @Det Object @Det [] {Arrays.asList(1, 2, 3)};
     }
 }
