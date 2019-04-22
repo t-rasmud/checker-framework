2ac7f1f5b4306b257f964bbcee0081d8e68369b9
Jason Waataja <jwaataja@cs.washington.edu>
2019-04-22 00:03:33 -0700

    Added tests for @PolyDet("up") feature

--- checker/tests/determinism/PolyUpTest.java
+++ checker/tests/determinism/PolyUpTest.java
@@ -5,4 +5,24 @@ public class PolyUpTest {
         @PolyDet("up") int x = 5;
         return x;
     }
+
+    // Dummy method that returns @PolyDet("down").
+    @PolyDet("down") String retPolyUp(@PolyDet PolyUpTest this) {
+        return null;
+    }
+
+    // Dummy method that returns @PolyDet("up").
+    @PolyDet("up") String retPolyDown(@PolyDet PolyUpTest this) {
+        return null;
+    }
+
+    // Tests that @PolyDet("up") is converted to @PolyDet.
+    @PolyDet String testPolyUpNonCollecstion(@PolyDet PolyUpTest p) {
+        return p.retPolyUp();
+    }
+
+    // Tests that @PolyDet("down") is not converted to @PolyDet.
+    @PolyDet("down") String testPolyDownNonCollection(@PolyDet PolyUpTest p) {
+        return p.retPolyDown();
+    }
 }
