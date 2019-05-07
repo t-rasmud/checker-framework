5946d48f82b8167d1f8ce09e152f3051501e6ef7
Suzanne Millstein <smillst@cs.washington.edu>
2019-05-07 10:34:04 -0700

    Add some more test cases.

--- checker/tests/tainting/TaintingPolyFields.java
+++ checker/tests/tainting/TaintingPolyFields.java
@@ -3,20 +3,21 @@ import org.checkerframework.checker.tainting.qual.PolyTainted;
 import org.checkerframework.checker.tainting.qual.Tainted;
 import org.checkerframework.checker.tainting.qual.Untainted;
 
-public class TaintingFieldPolymorphism {
+public class TaintingPolyFields {
     @PolyTainted Integer x;
     @PolyTainted List<@PolyTainted String> lst;
 
-    void test() {
-        @Tainted TaintingFieldPolymorphism obj = new @Tainted TaintingFieldPolymorphism();
+    // Access of poly fields outside of the declaring class.
+    static void test() {
+        @Tainted TaintingPolyFields obj = new @Tainted TaintingPolyFields();
         // :: error: (assignment.type.incompatible)
         @Untainted Integer myX = obj.x;
         // :: error: (assignment.type.incompatible)
         @Untainted List<@Untainted String> myLst = obj.lst;
         // :: warning: (cast.unsafe.constructor.invocation)
-        @Untainted TaintingFieldPolymorphism obj1 = new @Untainted TaintingFieldPolymorphism();
+        @Untainted TaintingPolyFields obj1 = new @Untainted TaintingPolyFields();
         @Untainted Integer myX1 = obj1.x;
-        TaintingFieldPolymorphism obj2 = new TaintingFieldPolymorphism();
+        TaintingPolyFields obj2 = new TaintingPolyFields();
         // :: error: (assignment.type.incompatible)
         @Untainted List<@Untainted String> myLst2 = obj2.lst;
     }
