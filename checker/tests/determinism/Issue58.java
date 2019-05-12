12fb63bf58e44e7f411136474f14f07a8991488b
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-05-12 12:22:20 -0700

    refactoring: Use getEffectiveAnnotation

---
+++ checker/tests/determinism/Issue58.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import org.checkerframework.checker.determinism.qual.*;
+
+public class Issue58 {
+    public Issue58(@PolyDet int a) {}
+
+    public static void f(@PolyDet int b) {
+        @Det Issue58 c = new Issue58(b);
+    }
+}
