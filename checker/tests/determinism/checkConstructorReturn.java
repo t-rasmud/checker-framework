0b589341367d00e4af4afc8c409e525eced646cd
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-05-13 12:04:02 -0700

    test case

--- checker/tests/determinism/checkConstructorReturn.java
+++ checker/tests/determinism/checkConstructorReturn.java
@@ -1,10 +1,25 @@
+import org.checkerframework.checker.determinism.qual.Det;
+import org.checkerframework.checker.determinism.qual.NonDet;
 import org.checkerframework.checker.determinism.qual.PolyDet;
 
-public class checkConstructorReturn {
-    public @PolyDet checkConstructorReturn() {}
+public class CheckConstructorReturn {
+    @NonDet Object nonDetField;
+    @Det Object detField;
+
+    public @PolyDet CheckConstructorReturn() {}
+    // :: warning: (inconsistent.constructor.type)
+    @Det CheckConstructorReturn(@NonDet Object nonDet, @Det Object det) {
+        nonDetField = nonDet;
+        detField = det;
+    }
 
     void test() {
         // :: error: (argument.type.incompatible)
-        System.out.println(new checkConstructorReturn());
+        System.out.println(new CheckConstructorReturn());
+    }
+
+    void test1(@NonDet Object nd, @Det Object d) {
+        CheckConstructorReturn x = new CheckConstructorReturn(nd, d);
+        System.out.println(x);
     }
 }
