c240a4f85ed7c10e5763e715c8c69819bc856731
Suzanne Millstein <smillst@cs.washington.edu>
2019-06-19 12:35:10 -0700

    Link to manual.

--- checker/src/main/java/org/checkerframework/checker/determinism/qual/CollectionType.java
+++ checker/src/main/java/org/checkerframework/checker/determinism/qual/CollectionType.java
@@ -1,13 +1,20 @@
 package org.checkerframework.checker.determinism.qual;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Type declaration annotation that indicates that the type is a collection of objects. The objects
  * may or may not be in a deterministic order.
+ *
+ * @checker_framework.manual #determinism-checker Determinism Checker
  */
 @Target(ElementType.TYPE)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface CollectionType {}
