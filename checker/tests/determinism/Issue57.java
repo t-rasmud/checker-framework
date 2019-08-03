b8b4ce4119aefc5ff7381ca5d88d50b693f32146
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-08-02 21:48:18 -0700

    Tests for #57

--- checker/tests/determinism/Issue57.java
+++ checker/tests/determinism/Issue57.java
@@ -1,8 +1,7 @@
-// @skip-test
 import java.util.*;
 import org.checkerframework.checker.determinism.qual.*;
 
-public class Issue57<T> implements @NonDet Iterator<T> {
+public @NonDet class Issue57<T> implements @NonDet Iterator<T> {
     public Issue57() {}
 
     public @PolyDet("down") boolean hasNext() {
