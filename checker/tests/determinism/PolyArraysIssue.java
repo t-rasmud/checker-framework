7c0f39f86c050bb30c18fb7adafbf6bff894e5d8
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-10-17 16:11:39 -0700

    test case

--- checker/tests/determinism/PolyArraysIssue.java
+++ checker/tests/determinism/PolyArraysIssue.java
@@ -1,19 +1,11 @@
-import java.util.ArrayList;
 import org.checkerframework.checker.determinism.qual.PolyDet;
 
 public class PolyArraysIssue {
     void myPolyInt(@PolyDet Integer arg) {}
 
-    void myPolyMethod(@PolyDet int arg) {}
-
-    public void equals(
-            @PolyDet Integer @PolyDet [] a,
-            @PolyDet ArrayList<@PolyDet Integer> l,
-            @PolyDet int index) {
+    public void equals(@PolyDet Integer @PolyDet [] a, @PolyDet int index) {
         @PolyDet("up") Integer arrayVal = a[index];
         myPolyInt(arrayVal);
         myPolyInt(a[index]);
-        @PolyDet("up") Integer listVal = l.get(index);
-        myPolyMethod(listVal);
     }
 }
