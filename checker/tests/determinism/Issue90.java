92a90f3683858944340e742eb880e38203dc2237
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-10-30 09:45:57 -0700

    #90 test case

---
+++ checker/tests/determinism/Issue90.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import org.checkerframework.checker.determinism.qual.*;
+
+public class Issue90 {
+    void test(@OrderNonDet HashMap<@Det String, @Det String> map) {
+        for (Iterator<Entry<String, String>> entries = map.entrySet().iterator();
+                entries.hasNext(); ) {
+            // :: error: (assignment.type.incompatible)
+            @Det Entry<@Det String, @Det String> item = entries.next();
+        }
+    }
+
+    void test1(@OrderNonDet HashMap<@Det String, @Det String> map) {
+        // :: error: (enhancedfor.type.incompatible)
+        for (@Det Entry<@Det String, @Det String> item : map.entrySet()) {}
+    }
+}
