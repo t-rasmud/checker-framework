a4720f2c306b2e29bc231e8628b2e341c4a834c9
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-11-05 09:51:37 -0800

    test case and JDk

---
+++ checker/tests/determinism/TestRemoveAddAll.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import org.checkerframework.checker.determinism.qual.Det;
+import org.checkerframework.checker.determinism.qual.OrderNonDet;
+
+public class TestRemoveAddAll {
+    void testRemove(@OrderNonDet ArrayList<@Det String> lst, @Det int index) {
+        // :: error: (method.invocation.invalid)
+        lst.remove(index);
+    }
+
+    void testAddall(
+            @OrderNonDet LinkedList<@Det String> lst,
+            @OrderNonDet LinkedList<@Det String> lst1,
+            @Det LinkedList<@Det String> lst2) {
+        // :: error: (argument.type.invalid)
+        lst.addAll(lst1);
+        lst.addAll(lst2);
+    }
+}
