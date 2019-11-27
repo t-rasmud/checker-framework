32d3130171b30de56553681c7a08305ea31f561e
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-11-27 14:23:48 -0800

    Check variable arguments against the last parameter type for @RequiresDetToString. Fixes #137.

--- checker/tests/determinism/Issue137.java
+++ checker/tests/determinism/Issue137.java
@@ -9,10 +9,29 @@ class Issue137 {
     public static void g(@Det Object er, String... args) {}
 
     @RequiresDetToString
-    public static void h(@Det ErrClass e) {
+    public static void i(Object... args) {}
+
+    public static void h(@Det ErrClass e, Object o) {
         f("a", "b");
         // :: error: (nondeterministic.tostring)
         g(e);
+        f();
+    }
+
+    public static void j(
+            @NonDet CorrectClass a, @Det CorrectClass b, @NonDet ErrClass c, @Det ErrClass d) {
+        i(a);
+        i(b);
+        i(a, b);
+        i(b, a);
+
+        i(c);
+        // :: error: (nondeterministic.tostring)
+        i(d);
+        // :: error: (nondeterministic.tostring)
+        i(c, d);
+        // :: error: (nondeterministic.tostring)
+        i(d, c);
     }
 }
 
@@ -20,5 +39,12 @@ class ErrClass {
     @Override
     public @NonDet String toString() {
         return super.toString();
+    }
+}
+
+class CorrectClass {
+    @Override
+    public @Det String toString() {
+        return "hi";
     }
 }
