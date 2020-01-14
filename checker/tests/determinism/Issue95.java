4fe3a9447435ab85547eb86bb1ac10886bd88b5e
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-01-14 14:38:23 -0800

    Adding test case for #95

---
+++ checker/tests/determinism/Issue95.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import org.checkerframework.checker.determinism.qual.Det;
+import org.checkerframework.checker.determinism.qual.NonDet;
+import org.checkerframework.checker.determinism.qual.OrderNonDet;
+
+public class Issue95 {
+    void test(@OrderNonDet List<@Det Class<@Det String>> classList, Object obj) {
+        @NonDet Class<@Det String> cl = classList.get(0);
+        @NonDet Class casted = cl.getClass().cast(obj);
+    }
+}
