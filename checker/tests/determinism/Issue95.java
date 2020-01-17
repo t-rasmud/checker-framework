cd6fd025d056961df4e29d511bec5f5fa74fc8ac
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-01-17 12:12:50 -0800

    test case

--- checker/tests/determinism/Issue95.java
+++ checker/tests/determinism/Issue95.java
@@ -6,6 +6,7 @@ import org.checkerframework.checker.determinism.qual.OrderNonDet;
 public class Issue95 {
     void test(@OrderNonDet List<@Det Class<@Det String>> classList, Object obj) {
         @NonDet Class<@Det String> cl = classList.get(0);
-        @NonDet Class casted = cl.getClass().cast(obj);
+        // :: error: (assignment.type.incompatible)
+        @Det Class casted = cl.getClass().cast(obj);
     }
 }
