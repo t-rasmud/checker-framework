61f22a99ecf0b1bf9f1046efce1f52bd4ff26473
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-01-17 16:14:24 -0800

    Correctly annotates Class cast; Fixes #95

--- checker/tests/determinism/Issue95.java
+++ checker/tests/determinism/Issue95.java
@@ -1,12 +1,9 @@
-import java.util.List;
 import org.checkerframework.checker.determinism.qual.Det;
 import org.checkerframework.checker.determinism.qual.NonDet;
-import org.checkerframework.checker.determinism.qual.OrderNonDet;
 
 public class Issue95 {
-    void test(@OrderNonDet List<@Det Class<@Det String>> classList, Object obj) {
-        @NonDet Class<@Det String> cl = classList.get(0);
+    void test(@NonDet Class<@Det String> cl, Object obj) {
         // :: error: (assignment.type.incompatible)
-        @Det Class casted = cl.getClass().cast(obj);
+        @Det String cast = cl.cast(obj);
     }
 }
