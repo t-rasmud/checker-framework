00cdf698b9b034d9c1e6b1ea117ff53d43da5a87
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-03-02 12:48:43 -0800

    map entrySet annotations: Fixes #164

---
+++ checker/tests/determinism/MapEntrySet.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.util.Set;
+import org.checkerframework.checker.determinism.qual.Det;
+import org.checkerframework.checker.determinism.qual.NonDet;
+import org.checkerframework.checker.determinism.qual.OrderNonDet;
+
+public class MapEntrySet {
+    void testNDMap(@NonDet Map<@NonDet String, @NonDet String> map) {
+        // :: error: (assignment.type.incompatible)
+        @Det Set<Map.@Det Entry<@NonDet String, @NonDet String>> entries = map.entrySet();
+    }
+
+    void testONDMap(@OrderNonDet Map<@Det String, @Det String> map) {
+        @OrderNonDet Set<Map.@Det Entry<@Det String, @Det String>> entries = map.entrySet();
+    }
+
+    void testDetMap(@Det Map<@Det String, @Det String> map) {
+        @Det Set<Map.@Det Entry<@Det String, @Det String>> entries = map.entrySet();
+    }
+}
