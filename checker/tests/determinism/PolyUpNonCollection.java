92b846752a2647837bdfa73bb0fd2c06c7613bf4
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-05-13 20:06:34 -0700

    test case

--- checker/tests/determinism/PolyUpNonCollection.java
+++ checker/tests/determinism/PolyUpNonCollection.java
@@ -1,5 +1,7 @@
 import java.util.List;
+import org.checkerframework.checker.determinism.qual.Det;
 import org.checkerframework.checker.determinism.qual.NonDet;
+import org.checkerframework.checker.determinism.qual.OrderNonDet;
 import org.checkerframework.checker.determinism.qual.PolyDet;
 import org.checkerframework.framework.qual.HasQualifierParameter;
 
@@ -17,5 +19,22 @@ public class PolyUpNonCollection {
     void collectionMethod1(@PolyDet List<@PolyDet Integer> lst, @PolyDet PolyUpNonCollection arg) {
         int x = lst.get(0);
         nonCollection1(x, arg);
+    }
+
+    void callCollectionMethod(
+            @PolyDet List<@PolyDet Integer> lst, @OrderNonDet List<@Det Integer> lst1) {
+        collectionMethod(lst);
+        collectionMethod(lst1);
+    }
+
+    static void collectionMethod2(@PolyDet ClassWithListField c) {
+        for (@PolyDet("up") String elt : c.list) {
+            @PolyDet String s = elt;
+        }
+    }
+
+    @HasQualifierParameter(NonDet.class)
+    public static class ClassWithListField {
+        @PolyDet List<@PolyDet String> list;
     }
 }
