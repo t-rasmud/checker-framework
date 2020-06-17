897b44b9678adfb793c752e856d77abb41d74ad1
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-06-17 15:11:36 -0700

    jdk 11 test

--- framework/tests/all-systems/VarKeyword.java
+++ framework/tests/all-systems/VarKeyword.java
@@ -2,17 +2,14 @@ import java.util.List;
 
 // @below-java11-jdk-skip-test
 public class VarKeyword {
-    @SuppressWarnings("dereference.of.nullable")
+    @SuppressWarnings({"dereference.of.nullable", "determinism:argument.type.incompatible"})
     void method(List<VarKeyword> list) {
         var s = "Hello!";
         s = null;
         s.toString();
         for (var i : list) {}
 
-        @SuppressWarnings({
-            "determinism:assignment.type.incompatible",
-            "determinism:argument.type.incompatible"
-        }) // TODO: fix
+        @SuppressWarnings({"determinism:assignment.type.incompatible"}) // TODO: fix
         var listVar = list;
         method((listVar));
     }
