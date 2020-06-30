0a7dfcddb547be315315ecd8dc8d4d07d697c1e2
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-06-29 18:12:03 -0700

    checksum library testcase

---
+++ checker/tests/checksum/ChecksumLibrary.java
@@ -0,0 +1,18 @@
+import java.util.function.Function;
+import javafx.util.Pair;
+
+public class ChecksumLibrary {
+    int computeChecksum(Object data) {
+        return 0;
+    }
+
+    Object validateChecksum(Object data, int checksum) {
+        return data;
+    }
+
+    Pair<Object, Integer> TransformChecksum(
+            Object data, int checksum, Function<Object, Object> lambda) {
+        Object newData = lambda.apply(data);
+        return new Pair(newData, 0);
+    }
+}
