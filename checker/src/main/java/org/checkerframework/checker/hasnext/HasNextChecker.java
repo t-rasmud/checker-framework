93c2b26d571de496002e82d4f81c18083112cf93
Michael Ernst <mernst@cs.washington.edu>
2020-10-28 21:58:52 -0700

    Add documentation

--- checker/src/main/java/org/checkerframework/checker/hasnext/HasNextChecker.java
+++ checker/src/main/java/org/checkerframework/checker/hasnext/HasNextChecker.java
@@ -2,4 +2,5 @@ package org.checkerframework.checker.hasnext;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
 
+/** The HasNext Checker ensures that Iterator.next() does not throw NoSuchElementException. */
 public class HasNextChecker extends BaseTypeChecker {}
