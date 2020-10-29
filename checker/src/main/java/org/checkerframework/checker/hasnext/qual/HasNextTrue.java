e0948d75a0b71b675d58f5c8bd71fe5f2ab8b97c
Michael Ernst <mernst@cs.washington.edu>
2020-10-29 07:56:45 -0700

    Fix Javadoc

--- checker/src/main/java/org/checkerframework/checker/hasnext/qual/HasNextTrue.java
+++ checker/src/main/java/org/checkerframework/checker/hasnext/qual/HasNextTrue.java
@@ -3,12 +3,12 @@ package org.checkerframework.checker.hasnext.qual;
 import java.lang.annotation.*;
 import org.checkerframework.framework.qual.*;
 
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-@SubtypeOf({UnknownHasNext.class})
 /**
  * An expression whose type has this annotation is an iterator that has a next value -- that is,
  * {@code next()} will not throw NoSuchElementException.
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@SubtypeOf({UnknownHasNext.class})
 public @interface HasNextTrue {}
