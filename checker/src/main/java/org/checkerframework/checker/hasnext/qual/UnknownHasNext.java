e3291ac5b3e77a3b6d4a92029aa54c8f5c5cb1f4
Michael Ernst <mernst@cs.washington.edu>
2020-10-29 08:31:42 -0700

    Fix location of comments

--- checker/src/main/java/org/checkerframework/checker/hasnext/qual/UnknownHasNext.java
+++ checker/src/main/java/org/checkerframework/checker/hasnext/qual/UnknownHasNext.java
@@ -3,6 +3,7 @@ package org.checkerframework.checker.hasnext.qual;
 import java.lang.annotation.*;
 import org.checkerframework.framework.qual.*;
 
+/** Either a non-iterator, or an iterator that might or might not have a next element. */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
@@ -10,5 +11,4 @@ import org.checkerframework.framework.qual.*;
 @DefaultQualifierInHierarchy
 @DefaultFor(value = TypeUseLocation.LOWER_BOUND, types = Void.class)
 @QualifierForLiterals(LiteralKind.NULL)
-/** Either a non-iterator, or an iterator that might or might not have a next element. */
 public @interface UnknownHasNext {}
