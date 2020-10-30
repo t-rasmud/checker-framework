7e9c12f20bbfccf7df1f2095bb95f20cce16bbae
Michael Ernst <mernst@cs.washington.edu>
2020-10-30 10:54:19 -0700

    Set sideEffectsUnrefineAliases (#209)

--- checker/src/main/java/org/checkerframework/checker/hasnext/HasNextChecker.java
+++ checker/src/main/java/org/checkerframework/checker/hasnext/HasNextChecker.java
@@ -1,6 +1,15 @@
 package org.checkerframework.checker.hasnext;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.framework.type.GenericAnnotatedTypeFactory;
 
 /** The HasNext Checker ensures that Iterator.next() does not throw NoSuchElementException. */
-public class HasNextChecker extends BaseTypeChecker {}
+public class HasNextChecker extends BaseTypeChecker {
+
+    @Override
+    public GenericAnnotatedTypeFactory<?, ?, ?, ?> getTypeFactory() {
+        GenericAnnotatedTypeFactory<?, ?, ?, ?> result = super.getTypeFactory();
+        result.sideEffectsUnrefineAliases = true;
+        return result;
+    }
+}
