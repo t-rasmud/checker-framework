3ab1a25f9ca9efd1909063855aefeec3182567f1
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-11-04 18:11:29 -0800

    merge

--- checker/src/test/java/org/checkerframework/checker/test/junit/StubparserTest.java
+++ checker/src/test/java/org/checkerframework/checker/test/junit/StubparserTest.java
@@ -2,27 +2,30 @@ package org.checkerframework.checker.test.junit;
 
 import java.io.File;
 import java.util.List;
+import org.checkerframework.checker.tainting.TaintingChecker;
 import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
-import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-public class StubparserTest extends CheckerFrameworkPerDirectoryTest {
+/** JUnit tests for stub parsing. */
+public class StubParserTest extends CheckerFrameworkPerDirectoryTest {
 
     /**
-     * Create a StubparserTest.
+     * Create a StubParserTest.
      *
      * @param testFiles the files containing test code, which will be type-checked
      */
-    public StubparserTest(List<File> testFiles) {
+    public StubParserTest(List<File> testFiles) {
         super(
                 testFiles,
-                org.checkerframework.checker.nullness.NullnessChecker.class,
-                "nullness",
+                TaintingChecker.class,
+                "stubparser",
                 "-Anomsgtext",
-                "-AstubWarnIfNotFound");
+                "-AmergeStubsWithSource",
+                "-Astubs=tests/stubparser/TypeParamWithInner.astub");
     }
 
-    @Parameterized.Parameters
+    @Parameters
     public static String[] getTestDirs() {
-        return new String[] {"stubparser-tests"};
+        return new String[] {"stubparser", "all-systems"};
     }
 }
