a1aca3dde1757a5c2d64dc26e9243a4497609086
Michael Ernst <mernst@cs.washington.edu>
2020-11-23 06:22:37 -0800

    Fix test

--- checker/tests/determinism/InvalidArrayComponentType.java
+++ checker/tests/determinism/InvalidArrayComponentType.java
@@ -7,7 +7,7 @@ import org.checkerframework.checker.determinism.qual.*;
 public class InvalidArrayComponentType {
 
     public static void methodForName(String method) {
-        @PolyDet("use") String @PolyDet [] bnArgnames = method.split(" *, *");
-        @PolyDet("use") String @PolyDet [] argnames = bnArgnames;
+        @PolyDet String @PolyDet [] bnArgnames = method.split(" *, *");
+        @PolyDet String @PolyDet [] argnames = bnArgnames;
     }
 }
