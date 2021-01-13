3699733a851bf449cab39409b02d5f3c09082b8b
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2021-01-12 16:24:27 -0800

    reformatting

--- checker/tests/determinism-det-default/NutzTest.java
+++ checker/tests/determinism-det-default/NutzTest.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.checkerframework.checker.determinism.qual.Det;
 import org.checkerframework.checker.determinism.qual.NonDet;
 
