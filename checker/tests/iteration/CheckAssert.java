b11065f617b584773aaa1f93a0fc0a7414967130
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2021-01-26 14:46:39 -0800

    assert test

---
+++ checker/tests/iteration/CheckAssert.java
@@ -0,0 +1,19 @@
+package iteration;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CheckAssert {
+    void test(Collection<Integer> coll) {
+        Iterator<Integer> iter = coll.iterator();
+        assert !iter.hasNext() : "@AssumeAssertion(iteration)";
+        // :: error: method.invocation.invalid
+        iter.next();
+    }
+
+    void test1(Collection<Integer> coll) {
+        Iterator<Integer> iter = coll.iterator();
+        assert iter.hasNext() : "@AssumeAssertion(iteration)";
+        iter.next();
+    }
+}
