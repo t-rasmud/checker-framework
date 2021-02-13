f1dc6576c99c7956952b5d74592f6a7a5633fb05
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2021-02-13 14:23:24 -0800

    test lub

---
+++ checker/tests/iteration/TestSizeLub.java
@@ -0,0 +1,17 @@
+package iteration;
+
+import java.util.List;
+
+public class TestSizeLub {
+    void test(List<Integer> lst, boolean cond) {
+        int size = 0;
+        if (cond) {
+            size = lst.size();
+            if (size == 9) {
+                lst.iterator().next();
+            }
+        }
+        // :: error: method.invocation.invalid
+        lst.iterator().next();
+    }
+}
