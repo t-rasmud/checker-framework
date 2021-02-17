ba2e4f4f9424f93dc87a9813876c44b4d36d05e8
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2021-02-16 16:57:38 -0800

    checker-qual for nonempty

--- checker-qual/src/main/java/org/checkerframework/checker/nonempty/UnknownNonEmpty.java
+++ checker-qual/src/main/java/org/checkerframework/checker/nonempty/UnknownNonEmpty.java
@@ -5,7 +5,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.checkerframework.framework.qual.*;
+import org.checkerframework.framework.qual.DefaultFor;
+import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
+import org.checkerframework.framework.qual.InvisibleQualifier;
+import org.checkerframework.framework.qual.LiteralKind;
+import org.checkerframework.framework.qual.QualifierForLiterals;
+import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 /**
  * Indicates that the collection, map, or iterator not known to be a non-empty. It is the top type
@@ -20,7 +26,7 @@ import org.checkerframework.framework.qual.*;
 @SubtypeOf({})
 @DefaultQualifierInHierarchy
 @DefaultFor({TypeUseLocation.LOWER_BOUND})
-@QualifierForLiterals(LiteralKind.NULL)
+@QualifierForLiterals({LiteralKind.NULL})
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
