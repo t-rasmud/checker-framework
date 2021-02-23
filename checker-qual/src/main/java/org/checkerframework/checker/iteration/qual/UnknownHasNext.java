8a066570c011e134276498a4f787bff0eef220e3
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2021-02-23 13:59:28 -0800

    imports refactoring

--- checker-qual/src/main/java/org/checkerframework/checker/iteration/qual/UnknownHasNext.java
+++ checker-qual/src/main/java/org/checkerframework/checker/iteration/qual/UnknownHasNext.java
@@ -1,7 +1,16 @@
 package org.checkerframework.checker.iteration.qual;
 
-import java.lang.annotation.*;
-import org.checkerframework.framework.qual.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.DefaultFor;
+import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
+import org.checkerframework.framework.qual.LiteralKind;
+import org.checkerframework.framework.qual.QualifierForLiterals;
+import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 /** Either a non-iterator, or an iterator that might or might not have a next element. */
 @Documented
