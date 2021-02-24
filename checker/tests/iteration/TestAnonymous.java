fbcc992afcf355d0cd2999d92567e5cdf332d180
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2021-02-24 09:48:23 -0800

    Enumeration test

---
+++ checker/tests/iteration/TestAnonymous.java
@@ -0,0 +1,26 @@
+package iteration;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import org.checkerframework.checker.iteration.qual.HasNext;
+
+public class TestAnonymous {
+    List fields = new ArrayList();
+
+    class MyEnumeration implements Enumeration {
+        final Iterator iter = fields.iterator();
+
+        @Override
+        @SuppressWarnings("contracts.conditional.postcondition.not.satisfied")
+        public boolean hasMoreElements() {
+            return iter.hasNext();
+        }
+
+        @Override
+        public Object nextElement(@HasNext MyEnumeration this) {
+            return iter.next();
+        }
+    }
+}
